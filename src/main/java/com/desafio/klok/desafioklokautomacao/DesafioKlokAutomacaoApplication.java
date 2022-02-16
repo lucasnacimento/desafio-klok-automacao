1cd704c3a84a2b6ea04f6034d9ea03a7ae83813f
Lucas Nascimento Lopes <85919181+lucasnacimento@users.noreply.github.com>
2022-02-16 11:35:08 -0300

    criando projeto

---
+++ src/main/java/com/desafio/klok/desafioklokautomacao/DesafioKlokAutomacaoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.klok.desafioklokautomacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKlokAutomacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKlokAutomacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/klok/desafioklokautomacao/DesafioKlokAutomacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.klok.desafioklokautomacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioKlokAutomacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
